8559f8c512d7edf9d3af173a539687b2b438f0fe
Lauraarodriguess <laura.rodriguess0563@gmail.com>
2025-06-01 20:51:34 -0300

    Add files via upload

---
+++ sricpt.js
@@ -0,0 +1,7 @@
+//js= Java script
+
+var nomes = ["Luana", "pedro", "Rafaela", "Mariana", "Kali", "Kailayne", "Samuel", "William","Pietro","vinicius", "Laura", "Sarah", "Rafaella", "Malu", "Clara", "Ana JU", "Marina"]
+
+var nomeEscolhido = parseInt(prompt("Digite um número de 0 a 14"))
+
+document.write("Nome escolhido:  " + nomes[nomeEscolhido])
